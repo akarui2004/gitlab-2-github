144793ea56bc49565a887e16a878f97eba0ee560
Minh Doan <minhdpc@gmail.com>
2025-11-12 19:38:17 +0700

    Added: interpolate string helper

---
+++ src/helpers/string.ts
@@ -0,0 +1,23 @@
+import type { InterpolateVars, InterpolateOptions } from '@/types';
+
+const interpolate = (
+  template: string,
+  vars: InterpolateVars,
+  options: InterpolateOptions = {}
+): string => {
+  const { defaultValue = '' } = options;
+
+  return template.replace(/\{([\w.]+)\}/g, (_, key: string): string => {
+    const keys = key.split(".");
+    let value = vars;
+
+    for (const k of keys) {
+      value = value?.[k];
+      if (value === undefined) return defaultValue;
+    }
+
+    return String(value ?? defaultValue);
+  })
+}
+
+export { interpolate };
---
+++ src/types/string.helper.type.ts
@@ -0,0 +1,7 @@
+type InterpolateVars = Record<string, any>;
+
+interface InterpolateOptions {
+  defaultValue?: string;
+}
+
+export type { InterpolateVars, InterpolateOptions };
