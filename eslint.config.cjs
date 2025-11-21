3b01e151d9adb8a3cb5c83ec82d9d9e4f5c42c5c
Minh Doan <minhdpc@gmail.com>
2025-11-21 23:53:30 +0700

    Added: Add eslint and prettier

---
+++ eslint.config.cjs
@@ -0,0 +1,33 @@
+// eslint.config.cjs
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      // Disable base ESLint rules that conflict with TypeScript
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+      // TypeScript ESLint recommended rules
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      // Prettier integration
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
